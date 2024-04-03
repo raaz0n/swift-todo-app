9b838cb8e130a18e4593dd849814f8fd9d53e9dd
rajan-poudel <rajan.rp50@gmail.com>
2024-04-03 16:52:02 +0545

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Fatdog on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
